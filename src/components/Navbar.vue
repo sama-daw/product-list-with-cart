6521b9c0dbd56d3324f4d235a91215b567f2cb57
SamaDaw <samadaw3@gmail.com>
2025-09-12 01:18:13 +0300

    added things to navpar

---
+++ src/components/Navbar.vue
@@ -0,0 +1,17 @@
+<template>
+  <nav class="navbar navbar-expand-lg navbar-light bg-light">
+    <div class="container">
+      <a class="navbar-brand" href="#">Shop</a>
+      <ul class="navbar-nav ms-auto">
+        <li class="nav-item"><a class="nav-link" href="#">Home</a></li>
+        <li class="nav-item"><a class="nav-link" href="#">Cart</a></li>
+      </ul>
+    </div>
+  </nav>
+</template>
+
+<script setup>
+</script>
+
+<style scoped>
+</style>
