b13c06d55da45b8e58fa409dfa19a823893f1f62
SamaDaw <samadaw3@gmail.com>
2025-09-12 05:14:36 +0300

    remove the navbar and added h1 , reduce the somk of the border fot add to cart button to 1px

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,28 @@
 <template>
-  <Navbar />
-  <ProductList />
+<div class="dessertsSection">
+  <h1> Desserts</h1>
+    <ProductList />
+   </div>
+
 </template>
 
 <script setup>
-import Navbar from './components/Navbar.vue'
 import ProductList from './components/ProductList.vue';
 
 </script>
 <style>
 @import './assets/styles/colors.css';
+.dessertsSection {
+  align-self: flex-start;
+  margin-top: 20px;
+  margin-left: 50px;
+  
+}
+h1{
+  color: var(--color-rose-900);
+  font-weight: 600;
+  margin-bottom: 10px;
+  margin-left: 20px;
+}
 </style>
 
