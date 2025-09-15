6c90041100ea8b17180fe298bb863b8261e1c4bc
SamaDaw <samadaw3@gmail.com>
2025-09-15 19:47:59 +0300

    used json file instead of creating object in product card

--- src/components/ProductCard.vue
+++ src/components/ProductCard.vue
@@ -12,28 +12,27 @@
         :alt="product.name"
       >
 
-     
+      <!-- زر Add to Cart -->
       <div v-if="quantity === 0">
         <button 
           class="btn add-to-cart-btn" 
           @click="addToCart"
         >
-        <svg xmlns="http://www.w3.org/2000/svg" width="21" height="20" fill="none" viewBox="0 0 21 20"><g fill="#C73B0F" clip-path="url(#a)"><path d="M6.583 18.75a1.25 1.25 0 1 0 0-2.5 1.25 1.25 0 0 0 0 2.5ZM15.334 18.75a1.25 1.25 0 1 0 0-2.5 1.25 1.25 0 0 0 0 2.5ZM3.446 1.752a.625.625 0 0 0-.613-.502h-2.5V2.5h1.988l2.4 11.998a.625.625 0 0 0 .612.502h11.25v-1.25H5.847l-.5-2.5h11.238a.625.625 0 0 0 .61-.49l1.417-6.385h-1.28L16.083 10H5.096l-1.65-8.248Z"/><path d="M11.584 3.75v-2.5h-1.25v2.5h-2.5V5h2.5v2.5h1.25V5h2.5V3.75h-2.5Z"/></g><defs><clipPath id="a"><path fill="#fff" d="M.333 0h20v20h-20z"/></clipPath></defs></svg>
+          <svg xmlns="http://www.w3.org/2000/svg" width="21" height="20" fill="none" viewBox="0 0 21 20"><g fill="#C73B0F" clip-path="url(#a)"><path d="M6.583 18.75a1.25 1.25 0 1 0 0-2.5 1.25 1.25 0 0 0 0 2.5ZM15.334 18.75a1.25 1.25 0 1 0 0-2.5 1.25 1.25 0 0 0 0 2.5ZM3.446 1.752a.625.625 0 0 0-.613-.502h-2.5V2.5h1.988l2.4 11.998a.625.625 0 0 0 .612.502h11.25v-1.25H5.847l-.5-2.5h11.238a.625.625 0 0 0 .61-.49l1.417-6.385h-1.28L16.083 10H5.096l-1.65-8.248Z"/><path d="M11.584 3.75v-2.5h-1.25v2.5h-2.5V5h2.5v2.5h1.25V5h2.5V3.75h-2.5Z"/></g><defs><clipPath id="a"><path fill="#fff" d="M.333 0h20v20h-20z"/></clipPath></defs></svg>
           Add to Cart
         </button>
       </div>
 
+      <!-- counter -->
       <div v-else class="counter-box">
-        <!-- زر ناقص -->   <button class="icon-btn" @click="decrease">
+        <button class="icon-btn" @click="decrease">
           <svg xmlns="http://www.w3.org/2000/svg" width="10" height="2" fill="none" viewBox="0 0 10 2">
             <path fill="#fff" d="M0 .375h10v1.25H0V.375Z"/>
           </svg>
         </button>
 
-      
         <span class="quantity">{{ quantity }}</span>
 
-       
         <button class="icon-btn" @click="increase">
           <svg xmlns="http://www.w3.org/2000/svg" width="10" height="10" fill="none" viewBox="0 0 10 10">
             <path fill="#fff" d="M10 4.375H5.625V0h-1.25v4.375H0v1.25h4.375V10h1.25V5.625H10v-1.25Z"/>
@@ -56,9 +55,13 @@
 <script setup>
 import { ref } from 'vue'
 
-defineProps({
+// props
+const props = defineProps({
   product: Object
 })
+
+// events
+const emit = defineEmits(['add-to-cart'])
 
 const quantity = ref(0)
 const isAnimating = ref(false)
@@ -66,6 +69,7 @@ const isAnimating = ref(false)
 function addToCart() {
   quantity.value = 1
   isAnimating.value = true
+  emit('add-to-cart', props.product) // ⬅️ هذا اللي يرسل للـ parent
   setTimeout(() => {
     isAnimating.value = false
   }, 500)
@@ -169,7 +173,6 @@ function decrease() {
   fill: var(--color-rose-400);
 }
 
-
 @keyframes pulse {
   0% { transform: scale(1); box-shadow: none; }
   50% { transform: scale(1.03); box-shadow: 0 0 20px rgba(199, 59, 15, 0.3); }
@@ -178,7 +181,6 @@ function decrease() {
 .animate-card {
   animation: pulse 0.5s ease;
 }
-
 
 .active-card {
   border: 1px solid var(--color-red);
